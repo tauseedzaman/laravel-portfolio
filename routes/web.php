e953b33963bf59ca0905eb4d3ba71300d8a1bcdb
tauseedzaman <tauseedzamanofsorana@gmail.com>
2023-08-23 14:19:06 +0100

    readme updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GeneralController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[GeneralController::class,"index"])->name("index");
-Route::get('/about',[GeneralController::class,"about"])->name("about");
-Route::get('/projects',[GeneralController::class,"projects"])->name("projects");
-Route::get('/photos',[GeneralController::class,"photos"])->name("photos");
+Route::get("/create", function () {
+    User::create([
+        "name" => "tauseed",
+        "name" => "tauseed@gmail.com",
+        "password" => bcrypt("password"),
+    ]);
+});
+
+Route::get('/', [GeneralController::class, "index"])->name("index");
+Route::get('/about', [GeneralController::class, "about"])->name("about");
+Route::get('/projects', [GeneralController::class, "projects"])->name("projects");
+Route::get('/photos', [GeneralController::class, "photos"])->name("photos");
